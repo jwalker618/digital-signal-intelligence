81425eaee7b9f6d6fa6709b7172aa9e46e749ca6
jwalker618 <johnea.walker@outlook.com>
2025-12-09 06:08:33 +0000

    Update aggregators.py

--- technical_pricing/signals/aggregators.py
+++ technical_pricing/signals/aggregators.py
@@ -2,7 +2,7 @@ from abc import ABC, abstractmethod
 from typing import Any, Dict, List, Type, Optional
 
 # ----------------------------
-# Registries
+# Registry
 # ----------------------------
 AGGREGATOR_REGISTRY: Dict[str, Type["DataAggregator"]] = {}
 
